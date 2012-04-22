70b7e3c2f75148a0437a3f8c89f62843ee22ad8c
Peter <ptrklly@gmail.com>
2012-04-21 20:16:02 -0700

    new polls folder

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,11 +5,11 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+    return render_to_response('index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p})
+    return render_to_response('detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
